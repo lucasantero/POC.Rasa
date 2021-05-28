80968f062c0a70666f0d75806ad3676d8111816b
Lucas Ferreira <lucas.ferreira@hubconexa.com>
2021-05-28 08:11:34 -0300

    grupo

---
+++ actions/grupoAction.py
@@ -0,0 +1,27 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+class ActionGrupo(Action):
+    def name(self) -> Text:
+        return "action_grupo"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        url = "https://9d8919557a17.ngrok.io/grupo"
+        response = requests.get(url)
+        dispatcher.utter_message(text=str(response.json()["Mensagem"]))
+
+        return []
